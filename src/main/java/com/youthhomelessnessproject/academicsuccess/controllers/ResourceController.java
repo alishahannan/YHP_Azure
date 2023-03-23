063cb1f799551467540e60afbd34fa95b3aebff3
alishahannan <40447851+alishahannan@users.noreply.github.com>
2023-03-23 00:08:46 -0400

    Updated Resource and Survey Controllers to base survey results on selected zip code

--- src/main/java/com/youthhomelessnessproject/academicsuccess/controllers/ResourceController.java
+++ src/main/java/com/youthhomelessnessproject/academicsuccess/controllers/ResourceController.java
@@ -1,25 +1,17 @@
 package com.youthhomelessnessproject.academicsuccess.controllers;
 
-import com.youthhomelessnessproject.academicsuccess.models.Employee;
-import com.youthhomelessnessproject.academicsuccess.models.Resource;
-import com.youthhomelessnessproject.academicsuccess.models.Session;
-import com.youthhomelessnessproject.academicsuccess.models.Student;
+import com.youthhomelessnessproject.academicsuccess.models.*;
+import com.youthhomelessnessproject.academicsuccess.services.AddressService;
 import com.youthhomelessnessproject.academicsuccess.services.ResourceService;
 import com.youthhomelessnessproject.academicsuccess.services.SessionService;
-import com.youthhomelessnessproject.academicsuccess.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 
 @Controller
 
@@ -30,7 +22,6 @@ public class ResourceController {
 
     @Autowired
     private ResourceService resourceService;
-
 
     // STUDENT RESOURCE METHODS
     @GetMapping("/resource/{id}")
@@ -60,7 +51,7 @@ public class ResourceController {
         List<Session> sessions = new ArrayList<>();
 
         for (Session session : sessionService.getAllSessionsByStudentId(student.getId())) {
-            if(session.getEnd_time() != null) {
+            if (session.getEnd_time() != null) {
                 sessions.add(session);
             }
         }
@@ -69,7 +60,6 @@ public class ResourceController {
         mav.addObject("sessions", sessions);
         return mav;
     }
-
 
     @GetMapping("/student/survey-results/{id}")
     public ModelAndView showStudentSurveyResult(@PathVariable Long id) {
@@ -84,13 +74,28 @@ public class ResourceController {
         double dependentScore = session.getDependentScore();
 
         List<Resource> allResources = new ArrayList<>();
+
         List<Resource> foodResources = resourceService.getAllFoodResourcesWithDegreeLessEqual(foodScore);
         List<Resource> housingResources = resourceService.getAllHousingResourcesWithDegreeLessEqual(housingScore);
         List<Resource> dependentResources = resourceService.getAllDependentResourcesWithDegreeLessEqual(dependentScore);
 
-        allResources.addAll(foodResources);
-        allResources.addAll(housingResources);
-        allResources.addAll(dependentResources);
+        for (Resource resource : foodResources) {
+            if ((resource.getAddress().getZip()).equals(session.getZipCode())) {
+                allResources.add(resource);
+            }
+        }
+
+        for (Resource resource : housingResources) {
+            if ((resource.getAddress().getZip()).equals(session.getZipCode())) {
+                allResources.add(resource);
+            }
+        }
+
+        for (Resource resource : dependentResources) {
+            if ((resource.getAddress().getZip()).equals(session.getZipCode())) {
+                allResources.add(resource);
+            }
+        }
 
         mav.addObject("student", student);
         mav.addObject("foodScore", foodScore);
@@ -106,5 +111,4 @@ public class ResourceController {
 
         return mav;
     }
-
 }
