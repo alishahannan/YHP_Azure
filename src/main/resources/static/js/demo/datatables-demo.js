f6957011488031faca8cec32fe7adbbdc01a8e86
alishahannan <40447851+alishahannan@users.noreply.github.com>
2023-04-25 19:31:26 -0400

    Updated resource name to be shown as hyperlink text instead of a button, removed details button on employee-resources-list.html, table display script from individual pages and added it to datatables-demo.js

--- src/main/resources/static/js/demo/datatables-demo.js
+++ src/main/resources/static/js/demo/datatables-demo.js
@@ -1,4 +1,9 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+    'columnDefs':[{
+      'targets':[-1],
+      'searchable':false
+    }]
+  });
 });
